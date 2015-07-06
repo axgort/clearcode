5d65f330de4be2dc4b8898f5f7dbcd93f12f5e62
havk <jastrzab5@gmail.com>
2015-07-06 21:22:46 +0200

    First version + unit test

--- dbTableCopier.py
+++ dbTableCopier.py
@@ -1,9 +1,85 @@
-def copyTable(sourceDB, destinationDB):
-    pass
+import sys
+import MySQLdb
+import datetime
+
+TYPE_STR = type('str')
+TYPE_DATETIME = type(datetime.date(2000, 1, 1))
+TYPE_NONE = type(None)
+
+
+def toStr(row):
+    res = []
+    for item in row:
+        if type(item) == TYPE_NONE:
+            res.append('NULL')
+        elif type(item) == TYPE_STR:
+            res.append('"'+item+'"')
+        elif type(item) == TYPE_DATETIME:
+            res.append('"'+str(item)+'"')
+        else:
+            res.append(str(item))
+
+    return ','.join(res)
+
+
+def insertRows(tableName, db, rows):
+    cur = db.cursor()
+
+    for row in rows:
+        vals = toStr(row)
+        try:
+            query = "INSERT INTO %s values(%s)" % (tableName, vals)
+            cur.execute(query)
+            db.commit()
+        except Exception, e:
+            print str(e)
+            sys.exit()
+
+
+def getRows(tableName, db):
+    cur = db.cursor()
+    query = "SELECT * FROM %s;" % tableName
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    return rows
+
+
+def copyTable(tableName, sourceDb, destinationDb):
+    rows = getRows(tableName, sourceDb)
+    insertRows(tableName, destinationDb, rows)
+
+
+def getParams(argv):
+    params = {
+        'tableName': argv[1],
+        'ipAddress1': argv[2],
+        'user1': argv[3],
+        'passwd1': argv[4],
+        'dbName1': argv[5],
+        'ipAddress2': argv[6],
+        'user2': argv[7],
+        'passwd2': argv[8],
+        'dbName2': argv[9],
+    }
+
+    return params
 
 
 def main():
-    pass
+    prms = getParams(sys.argv)
+
+    print prms
+
+    sourceDb = MySQLdb.connect(host=prms['ipAddress1'], user=prms['user1'],
+                               passwd=prms['passwd1'], db=prms['dbName1'])
+    destinationDb = MySQLdb.connect(host=prms['ipAddress2'], user=prms['user2'],
+                                    passwd=prms['passwd2'], db=prms['dbName2'])
+
+    copyTable(prms['tableName'], sourceDb, destinationDb)
+
+    sourceDb.close()
+    destinationDb.close()
 
 
 if __name__ == '__main__':
---
+++ unitTest.py
@@ -0,0 +1,62 @@
+import unittest
+import datetime
+from dbTableCopier import copyTable
+
+
+class DbStub(object):
+    def cursor(self):
+        self.cursor = CursorStub()
+        return self.cursor
+
+    def close(self):
+        pass
+
+    def commit(self):
+        pass
+
+
+class CursorStub(object):
+    def __init__(self):
+        self.inserts = []
+        self.selectWasCalled = 0
+
+    def execute(self, query):
+        if query[:13] == 'SELECT * FROM':
+            self.selectWasCalled = 1
+
+        if query[:6] == 'INSERT':
+            self.inserts.append(query)
+
+    def fetchall(self):
+        if self.selectWasCalled:
+            return [
+                (10001L, 'Senior Engineer',
+                 datetime.date(1986, 6, 26), datetime.date(9999, 1, 1)),
+                (10002L, 'Staff',
+                 datetime.date(1996, 8, 3), datetime.date(9999, 1, 1)),
+                (10004L, 'Engineer',
+                 datetime.date(1986, 12, 1), datetime.date(1995, 12, 1))]
+
+
+class TestDbTableCopier(unittest.TestCase):
+    def setUp(self):
+        self.sourceDb = DbStub()
+        self.destinationDb = DbStub()
+
+    def test_copyTable(self):
+        copyTable('titles', self.sourceDb, self.destinationDb)
+
+        inserts = ['INSERT INTO titles values(10001,"Senior Engineer",\
+"1986-06-26","9999-01-01")',
+                   'INSERT INTO titles values(10002,"Staff",\
+"1996-08-03","9999-01-01")',
+                   'INSERT INTO titles values(10004,"Engineer",\
+"1986-12-01","1995-12-01")']
+
+        self.assertEqual(self.destinationDb.cursor.inserts[0], inserts[0])
+        self.assertEqual(self.destinationDb.cursor.inserts[1], inserts[1])
+        self.assertEqual(self.destinationDb.cursor.inserts[2], inserts[2])
+
+
+if __name__ == '__main__':
+    unittest.main()
